96a233e4936576221b113c74a58f288949136ffa
Buzruk Toshkentov <buzruk@gmail.com>
2026-04-09 10:38:04 +0500

    feat: add UserCreateAction with password hashing

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$kernel = new Kernel('dev', true);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,13 +6,3 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class BookController extends AbstractController
-{
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
-    {
-        return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
-        ]);
-    }
-}
